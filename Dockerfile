3ecb464a74104051f25c61b6bbd63407161d667b
jazmindestefanogm <jazmin.destefano@gm2dev.com>
2025-03-06 18:51:34 -0300

    update dockerfile ndadd lambda

--- Dockerfile
+++ Dockerfile
@@ -3,14 +3,17 @@ FROM node:20
 # Establece el directorio de trabajo como /app
 WORKDIR /app
 
-# Copia los archivos del proyecto al directorio /app
-COPY . .
+# Copia solo el package.json y package-lock.json (o yarn.lock)
+COPY package*.json ./
 
 # Instala las dependencias
 RUN npm install
 
-# Define el comando para ejecutar la aplicación
-CMD ["node", "src/index.js"]
+# Copia el resto de los archivos
+COPY . .
 
 # Expone el puerto 3000
 EXPOSE 3000
+
+# Define el comando para ejecutar la aplicación
+CMD ["node", "src/index.js"]
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import AWS from 'aws-sdk';
 
 const app = express();
+const lambda = new AWS.Lambda({ region: 'us-east-1' });
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -8,6 +10,22 @@ app.get('/', (req, res) => {
 
 app.get('/health', (req, res) => {
     res.status(200).send('OK');
+});
+
+app.get('/invoke-lambda', (req, res) => {
+    const params = {
+        FunctionName: 'express-api',
+        InvocationType: 'RequestResponse',
+        Payload: JSON.stringify({})
+    };
+
+    lambda.invoke(params, (err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.send(data.Payload);
+        }
+    });
 });
 
 app.listen(3000, () => {
